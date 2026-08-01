10e7cecc08c1bbd683eded76cfacc00005e45335
ipeeyushsachan <peeyushsachan08@google.com>
2026-08-02 02:21:43 +0530

    [new] Oops Concept learning started

---
+++ src/take_u_forward/oops/OopsBasic.java
@@ -0,0 +1,38 @@
+package take_u_forward.oops;
+
+public class OopsBasic {
+    public static void main(String[] args) {
+        Student student1 = new Student();
+
+        student1.assignAge(10);
+
+        System.out.println(student1.age);
+        System.out.println(student1.height);
+        System.out.println(student1.name);
+
+        OopsBasic sc = new OopsBasic();
+        sc.println();
+    }
+    public void println(){
+        System.out.println("sum");
+    }
+}
+
+class Student {
+    int age;
+    int height;
+    String name;
+
+    public void assignAge(int age) {
+        this.age = age;
+    }
+
+    public void assignHeight(int height) {
+        this.height = height;
+    }
+
+    public void assignName(String name) {
+        this.name = name;
+    }
+}
+
